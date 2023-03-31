0d023ddc521396696b8c360a9b732f995cc2b556
Juliana Lucca <lucca.editora@gmail.com>
2023-03-31 17:18:03 -0300

    feat: add book sorting functionality and book availability

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -4,10 +4,13 @@ function exibirOsLivrosNaTela(listaDeLivros) {
   elementoParaInserirLivros.innerHTML = '';
   
   listaDeLivros.forEach(livro => {
+
+    let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livros__imagens indisponivel';
+
     elementoParaInserirLivros.innerHTML += 
     `
     <div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}"/>
+      <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}"/>
       <h2 class="livro__titulo">
         ${livro.titulo}
       </h2>
